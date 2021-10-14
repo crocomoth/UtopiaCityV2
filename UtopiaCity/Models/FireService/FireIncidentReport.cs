7c373261797cebab0e8a7cc56d9bf88d7185b288
Umirkhanov Kuanysh <umirkhanov.k@halykSK.kz>
2021-10-14 16:41:25 +0600

    Fire incident report model was added

---
+++ UtopiaCity/Models/FireService/FireIncidentReport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UtopiaCity.Models.FireService
+{
+    public class FireIncidentReport
+    {
+        public string Id { get; set; }
+        public DateTime IncidentDate { get; set; }
+        public string Address { get; set; }
+        public string EyewitnessId { get; set; }
+    }
+}
