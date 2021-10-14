8011fb4339dd32a2f6f496dbac3b332b1add5e8c
Umirkhanov Kuanysh <umirkhanov.k@halykSK.kz>
2021-10-14 17:12:26 +0600

    Migrations for fire incident report were added

---
+++ UtopiaCity/Data/Migrations/20211014105837_FireIncidentReportTableAdded.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UtopiaCity.Data.Migrations
+{
+    public partial class FireIncidentReportTableAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FireIncidentReports",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    IncidentDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EyewitnessId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FireIncidentReports", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FireIncidentReports");
+        }
+    }
+}
