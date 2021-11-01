a2b3cc3399d4f9ec1c2cb14454bb7b602f7c67e9
umirkhanov.k <umirkhanov.k@halykSK.kz>
2021-11-01 11:23:52 +0600

    Incident date default value to database config and migrations for config application were added

---
+++ UtopiaCity/Data/Migrations/20211030062254_IncidentDateFieldConfigAdded.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UtopiaCity.Data.Migrations
+{
+    public partial class IncidentDateFieldConfigAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "IncidentDate",
+                table: "FireIncidentReports",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETUTCDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "IncidentDate",
+                table: "FireIncidentReports",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETUTCDATE()");
+        }
+    }
+}
