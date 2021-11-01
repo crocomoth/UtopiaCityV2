6f2c8dfa1003fb1c0f53af58497ab4b21162a359
umirkhanov.k <umirkhanov.k@halykSK.kz>
2021-11-01 11:25:55 +0600

    Automapper profiles were added

---
+++ UtopiaCity/MappingConfigurations/FireDepartment/FireIncidentReportViewModelProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using UtopiaCity.Models.FireDepartment;
+using UtopiaCity.ViewModels.FireDepartment;
+
+namespace UtopiaCity.MappingConfigurations.FireDepartment
+{
+    public class FireIncidentReportViewModelProfile : Profile
+    {
+        public FireIncidentReportViewModelProfile()
+        {
+            CreateMap<FireIncidentReport, FireIncidentReportViewModel>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(o => o.Id))
+                .ForMember(dest => dest.Address, opt => opt.MapFrom(o => o.Address))
+                .ForMember(dest => dest.AdditionalInfo, opt => opt.MapFrom(o => o.AdditionalInfo))
+                .ForMember(dest => dest.IncidentDate, opt => opt.MapFrom(o => o.IncidentDate));
+        }
+    }
+}
