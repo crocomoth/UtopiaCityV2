34bb02f45819c00c49526ec1afc49c209f5da9ca
umirkhanov.k <umirkhanov.k@halykSK.kz>
2021-11-01 11:27:29 +0600

    Service for handling with fire incident reports was added

---
+++ UtopiaCity/Services/FireDepartment/FireIncidentReportServices/FireIncidentReportService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using System.Collections.Generic;
+using System.Linq;
+using UtopiaCity.Data;
+using UtopiaCity.Models.FireDepartment;
+using UtopiaCity.ViewModels.FireDepartment;
+
+namespace UtopiaCity.Services.FireDepartment.FireIncidentReportServices
+{
+    public class FireIncidentReportService : IFireIncidentReportService
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public FireIncidentReportService(
+            AppDbContext context,
+            IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public void CreateFireIncidentReport(CreateFireIncidentReportViewModel viewModel)
+        {
+            var fireIncidentReport = _mapper.Map<FireIncidentReport>(viewModel);
+            _context.Add(fireIncidentReport);
+            _context.SaveChanges();
+        }
+
+        public List<FireIncidentReportViewModel> GetAllFireIncidentReportViewModels()
+        {
+            var reports = _context.FireIncidentReports.ToList();
+            var viewModels = _mapper.Map<List<FireIncidentReportViewModel>>(reports);
+            return viewModels;
+        }
+    }
+}
