6db9c4520a7dd6d24b6c5650b8f4217d4fc58199
crocomoth <justletmeplaymf@gmail.com>
2021-11-29 21:23:03 +0300

    add viewcomponents and other materials from lesson

---
+++ UtopiaCity/Common/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace UtopiaCity.Common.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static HtmlString CreateList(this IHtmlHelper html, string[] items)
+        {
+            var tagBuilder = new TagBuilder("ul");
+            foreach (var item in items)
+            {
+                var innerTagBuilder = new TagBuilder("li");
+                innerTagBuilder.InnerHtml.Append(item);
+                tagBuilder.InnerHtml.AppendHtml(innerTagBuilder);
+            }
+
+            tagBuilder.Attributes.Add("class", "list");
+            var writer = new StringWriter();
+            tagBuilder.WriteTo(writer, HtmlEncoder.Default);
+            return new HtmlString(writer.ToString());
+        }
+    }
+}
---
+++ UtopiaCity/ViewComponents/LoggedInViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using UtopiaCity.Models.Common;
+
+namespace UtopiaCity.ViewComponents
+{
+    [ViewComponent(Name = "LoggedIn")]
+    public class LoggedInViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string loginData;
+            if (HttpContext.User != null && HttpContext.User.Identity.IsAuthenticated)
+            {
+                loginData = $"Logged in as: {HttpContext.User.Identity.Name}";
+            }
+            else
+            {
+                loginData = "You are not logged in!";
+            }
+
+            var model = new StringDataModel() { Data = loginData };
+            return View("LoggedInView", model);
+        }
+    }
+}
