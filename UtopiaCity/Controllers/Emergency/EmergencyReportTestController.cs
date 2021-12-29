7f5444b056a5a653433adfcc322dd3d4040483eb
Yauheni Papiunenka <justletmeplaymf@gmail.com>
2021-12-29 17:06:44 +0300

    last changes

---
+++ UtopiaCity/Controllers/Emergency/EmergencyReportTestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UtopiaCity.Controllers.Emergency
+{
+    public class EmergencyReportTestController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View("TestView");
+        }
+
+        [HttpPost]
+        public ActionResult SomeAction()
+        {
+            return Ok("From server");
+        }
+    }
+}
